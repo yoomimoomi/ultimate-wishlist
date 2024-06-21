c0908b59f5b33473b0d6644272896f8ae79a9457
Ash <150209573+yoomimoomi@users.noreply.github.com>
2024-06-21 18:38:01 -0400

    updated interface
    unstylized

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let total = null
+let itemName = null
+let descInput = null
+let linkInput = null
 
-// let entry = {
-//     total: total;
+let entry = {
+    item : itemName,
+    description : descInput,
+    link : linkInput
+}
 
-// };
+function removeEntry () {}
+
+
+function addEntry (){
+
+}
+
+window.location.href = "findjobs.html";
