825e48f3e7edfe2a0e601dd3e9c9f0fb3c04a9a5
Karina-Lam <Karinalam39@gmail.com>
2024-06-25 17:58:24 -0400

    Updated tagScript.js comment

--- tagScript.js
+++ tagScript.js
@@ -4,10 +4,8 @@ input = u1.querySelector("input");
 console.log("help!!");
 
 function addTag(e){
-    if(e.key == "Enter"){ // 10:34, doesn't print out on console
+    if(e.key == "Enter"){ // 10:34, enter key is different on mac (option + return) and windows.
         console.log("Enter key pressed"); 
-        //enter key is different on mac and windows?? when changing e.key to shift it works...
-        // https://discussions.apple.com/thread/7415318?sortBy=best
         console.log(e.target.value);
     }
 }
